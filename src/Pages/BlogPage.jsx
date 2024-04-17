b02919ff5dc31c17c08c945150968f60d5d451c2
619Saurabh <160112390+619Saurabh@users.noreply.github.com>
2024-04-17 17:19:12 +0530

    Update BlogPage.jsx

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -13,8 +13,8 @@ const BlogPage = () => {
  
   const [blog, setBlog] = useState(null);
   const[relatedBlogs, setRelatedBlogs] = useState([]);
-  const location = useLocation();//using useLocation() hook to extract the value of blogId from current location or current URL
-  const navigation = useNavigate();//using useNavigate() hook to navigate to back page
+  const location = useLocation();
+  const navigation = useNavigate();
 
  //Consuming data from context(AppContext) using useContext hook
   const {loading, setLoading} = useContext(AppContext);
@@ -23,10 +23,7 @@ const BlogPage = () => {
   //Extracting the value of blogId which is the last path in current location or current URL
 
   async function fetchRelatedBlogs(){
-    //If we are calling an API then before that we have to show loader(spinner) in UI for that we have to set loading state variable as true
     setLoading(true);
-
-    
     let url = `${newBaseUrl}get-blog?blogId=${blogId}`;  
     
     // console.log("URL is:");
@@ -35,11 +32,10 @@ const BlogPage = () => {
     //Calling API
     try{
 
-      const res = await fetch(url);//API call or Hitting GET request on API
-      const data = await res.json();//Converting the response coming from API call (which is in string or JSON string format) into JSON format 
-                                 //using json() method
-                          
-      console.log("Api Response", data);                       
+      const res = await fetch(url);
+      const data = await res.json();
+                    
+      // console.log("Api Response", data);                       
       setBlog(data.blog);   
       setRelatedBlogs(data.relatedBlogs);  
       
@@ -52,22 +48,19 @@ const BlogPage = () => {
       setBlog(null);
       setRelatedBlogs([]);
     }
-
-    //If API call is completed then remove the loader or spinner from UI
+    //After API call is completed then removing the loader 
     setLoading(false);
 
   }
   
   useEffect(() => {
-    if(blogId){//if we have blogId available then we will call async function fetchRelatedBlogs() to call an API on the basis of blogId
+    if(blogId){//if we have blogId available then we are calling async function fetchRelatedBlogs() to call an API on the basis of blogId
     
       fetchRelatedBlogs();
       
     }
 
   },[location.pathname])
-  //So, whenever there is a change in path(i.r blogId) of location or current URL then this useEffect() will be triggered to call async 
-  //function fetchRelatedBlogs() which calls an API on the basis of blogId
 
   return (
     <div className='w-full h-full flex flex-col gap-y-1 items-center justify-center mb-[50px]'>
@@ -85,7 +78,7 @@ const BlogPage = () => {
           (<div>
             <p>Loading</p>
           </div>)
-           : //if loading state variable is false then checks if we have the data of current blog in blog state variable
+           : //if loading is false then checking if we have the data of current blog 
           blog ?  //if current blog is available then render data of current blog and related blogs
            (
             <div>
@@ -97,9 +90,6 @@ const BlogPage = () => {
                 
                 {
                   relatedBlogs.map((post) => (
-                  //for each post or blog in relatedBlogs array we are creating a <div> and inside it we are rendering a card/card component 
-                  //i.e <BlogDetails/> component inside which we will pass the data of current post or blog from relatedBlogs array through
-                  // props to render in UI
                     <div key={post.id}> 
                        <BlogDetails post={post}/>
                     </div>
